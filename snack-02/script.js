74b9b0427d71c3e9d9b2aa5945b28904ea2438c0
eduardorocco <eduardo_rocco@hotmail.it>
2024-10-27 01:48:32 +0200

    ADD array and random function

--- snack-02/script.js
+++ snack-02/script.js
@@ -5,3 +5,96 @@
 //Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e
 //falli subiti e stampiamo tutto in console.
+
+const serieA = [
+    {
+
+        name: 'Napoli',
+
+        points: 0,
+
+        fouls: 0
+
+    },
+    {
+
+        name: 'Inter',
+
+        points: 0,
+
+        fouls: 0
+    },
+    {
+
+        name: 'Juventus',
+
+        points: 0,
+
+        fouls: 0
+
+    },
+    {
+
+        name: 'Milan',
+
+        points: 0,
+
+        fouls: 0
+
+    },
+    {
+
+        name: 'Genoa',
+
+        points: 0,
+
+        fouls: 0
+    },
+    {
+
+        name: 'Cagliari',
+
+        points: 0,
+
+        fouls: 0
+
+    },
+    {
+
+        name: 'Bologna',
+
+        points: 0,
+
+        fouls: 0
+
+    },
+    {
+
+        name: 'Firenze',
+
+        points: 0,
+
+        fouls: 0
+    },
+    {
+
+        name: 'Como',
+
+        points: 0,
+
+        fouls: 0
+
+    }
+]
+
+function randomNum50() {
+
+    return Math.floor(Math.random() * 50) + 1
+}
+
+// console.log(serieA)
+
+for (let i = 0; i < serieA.length; i++) {
+
+    console.log(i)
+}
